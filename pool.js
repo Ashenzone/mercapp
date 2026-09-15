fecf09944ee89f6bf519e2be711896e3856642cf
Ashenzone <duduxlfffoficial@gmail.com>
2026-09-15 13:46:02 -0300

    Add files via upload

--- pool.js
+++ pool.js
@@ -1,2 +1,1 @@
- const { Pool } = require('pg'); const url = process.env.DATABASE_URL || ''; const isLocal = /localhost|127\.0\.0\.1/.test(url); const pool = new Pool({ connectionString: url, ssl: url && !isLocal ? { rejectUnauthorized: false } : false, }); module.exports = { pool };
-
+const { Pool } = require('pg'); const url = process.env.DATABASE_URL || ''; const isLocal = /localhost|127\.0\.0\.1/.test(url); const pool = new Pool({ connectionString: url, ssl: url && !isLocal ? { rejectUnauthorized: false } : false, }); module.exports = { pool };
